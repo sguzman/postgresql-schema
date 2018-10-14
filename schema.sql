bdbe385c8d04bc8c28981a12f08a9207522a51a6
Salvador Guzman <guzmansalv@gmail.com>
2018-10-14 02:21:53 -0700

    dont forget connect

--- schema.sql
+++ schema.sql
@@ -1,17 +1,18 @@
 CREATE database youtube;
+\connect youtube
 CREATE SCHEMA entities;
 
-CREATE TABLE entities.channels(
+CREATE TABLE youtube.entities.channels(
   id bigserial PRIMARY KEY NOT NULL,
   serial char(24) NOT NULL
 );
-CREATE UNIQUE INDEX channels_id_uindex ON entities.channels (id);
-CREATE UNIQUE INDEX channels_serial_uindex ON entities.channels (serial);
+CREATE UNIQUE INDEX channels_id_uindex ON youtube.entities.channels (id);
+CREATE UNIQUE INDEX channels_serial_uindex ON youtube.entities.channels (serial);
 
-CREATE TABLE entities.videos(
+CREATE TABLE youtube.entities.videos(
   id bigserial NOT NULL,
   serial char(11) PRIMARY KEY NOT NULL,
-  CONSTRAINT videos_channels_id_fk FOREIGN KEY (id) REFERENCES entities.channels (id)
+  CONSTRAINT videos_channels_id_fk FOREIGN KEY (id) REFERENCES youtube.entities.channels (id)
 );
 
-CREATE UNIQUE INDEX videos_serial_uindex ON entities.videos (serial);
+CREATE UNIQUE INDEX videos_serial_uindex ON youtube.entities.videos (serial);
