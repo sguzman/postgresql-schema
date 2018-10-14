7d380305ef355247fe6bdb879512ab5c8d0da86d
Salvador Guzman <guzmansalv@gmail.com>
2018-10-14 02:07:45 -0700

    working sql script

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
-CREATE DATABASE youtube;
-USE youtube;
+CREATE database youtube;
 CREATE SCHEMA entities;
+
+CREATE TABLE entities.channels(
+  id bigserial PRIMARY KEY NOT NULL,
+  serial char(24) NOT NULL
+);
+CREATE UNIQUE INDEX channels_id_uindex ON entities.channels (id);
+CREATE UNIQUE INDEX channels_serial_uindex ON entities.channels (serial);
+
 CREATE TABLE entities.videos(
   id bigserial NOT NULL,
   serial char(11) PRIMARY KEY NOT NULL,
@@ -8,11 +15,3 @@ CREATE TABLE entities.videos(
 );
 
 CREATE UNIQUE INDEX videos_serial_uindex ON entities.videos (serial);
-
-CREATE TABLE entities.channels(
-id bigserial PRIMARY KEY NOT NULL,
-serial char(24) NOT NULL
-);
-CREATE UNIQUE INDEX channels_id_uindex ON entities.channels (id);
-CREATE UNIQUE INDEX channels_serial_uindex ON entities.table_name (serial);
-ALTER USER postgres WITH ENCRYPTED PASSWORD 'admin';
