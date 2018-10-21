acbf1acd21c94de76835072e792bae5493dfa2b3
Salvador Guzman <guzmansalv@gmail.com>
2018-10-20 18:01:32 -0700

    add chans table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,34 @@ CREATE VIEW counts AS
   SELECT
          (SELECT count(*) FROM youtube.entities.videos) vids,
          (SELECT count(*) FROM youtube.entities.channels) chans;
+
+
+CREATE TABLE youtube.entities.chans
+(
+	id BIGSERIAL NOT NULL CONSTRAINT chans_pkey PRIMARY KEY,
+	serial CHAR(24) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	custom_url VARCHAR(100),
+	description VARCHAR(8000),
+	joined TIMESTAMP NOT NULL,
+	thumbnail VARCHAR(400),
+	topic_ids VARCHAR(10) [],
+	topic_categories VARCHAR(200) [],
+	privacy_status VARCHAR(10) NOT NULL,
+	is_linked BOOLEAN NOT NULL,
+	long_uploads VARCHAR(25) NOT NULL,
+	tracking_id VARCHAR(25),
+	moderate_comments BOOLEAN,
+	show_related_channels BOOLEAN,
+	show_browse BOOLEAN,
+	banner_image VARCHAR(400) NOT NULL,
+	subs BIGINT NOT NULL,
+	video_count INT NOT NULL,
+	video_views BIGINT NOT NULL
+);
+
+CREATE UNIQUE index chans_id_uindex
+	ON entities.chans (id);
+
+CREATE UNIQUE index chans_chan_serial_uindex
+	ON entities.chans (serial);
