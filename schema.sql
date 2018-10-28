763027d767172bccb717bddd4a50f28ef7193451
Salvador Guzman <guzmansalv@gmail.com>
2018-10-28 04:18:49 -0700

    add timescaledb here

--- schema.sql
+++ schema.sql
@@ -28,3 +28,14 @@ CREATE TABLE youtube.entities.chans(
 );
 
 CREATE UNIQUE INDEX chans_chan_serial_uindex ON youtube.entities.chans (serial);
+
+CREATE TABLE youtube.entities.chan_stats
+(
+	time timestamptz DEFAULT now() NOT NULL,
+	serial char(24) NOT NULL,
+	subs bigint NOT NULL,
+	videos int NOT NULL,
+	views bigint NOT NULL
+);
+
+SELECT create_hypertable('youtube.entities.chan_stats', 'time');
